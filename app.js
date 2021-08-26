4a62cf88f912f57010e82151dd0e3d4222403187
zhuocaili <li.zhuoca@northeastern.edu>
2021-08-27 00:41:53 +0800

    changes

--- app.js
+++ app.js
@@ -133,6 +133,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-app.listen(3000, () => {
-    console.log("Serving on port 3000");
+//80 for heroku
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
 });
